848995685e550eb78fe974ad36cbcbd65e3f11ff
Satyam Singh <satyamsingh5august@gmail.com>
2024-07-12 23:18:10 +0530

    home page width fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -27,11 +27,11 @@ export default async function Home() {
   return (
     <>
       <Navbar />
-      <div className="w-screen min-h-screen bg-gradient-to-r from-rose-100 to-teal-100">
+      <div className="min-h-screen bg-gradient-to-r from-rose-100 to-teal-100">
         <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
           <div className="flex flex-col items-center text-center">
             <div className="flex items-center">
-              <h1 className="mr-3 text-5xl font-semibold">ChatWithPDF</h1>
+              <h1 className="mr-3 text-5xl font-bold">GenieAI</h1>
               <UserButton afterSignOutUrl="/" />
             </div>
             <div className="flex m-2 items-center justify-center">
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,23 +2,33 @@ import React from "react";
 import Link from "next/link";
 import { Button } from "./ui/button";
 import { LogIn } from "lucide-react";
+import { UserButton } from "@clerk/nextjs";
+import { auth } from "@clerk/nextjs/server";
 
 type Props = {};
 
-const Navbar = (props: Props) => {
+const Navbar = async (props: Props) => {
+
+    const { userId } = await auth();
+    const isAuth = !!userId
+
     return (
-        <div className="bg-[rgb(15,23,42)] z-10 flex flex-row justify-evenly items-center p-3">
+        <div className="bg-[rgb(15,23,42)] flex flex-row justify-evenly items-center py-4">
             <div className="text-2xl font-semibold text-white">GenieAI</div>
             <div className="center">
                 {/* <Button>Subscribe</Button> */}
             </div>
-            <div className="right">
-                <Link href="/signup">
-                    <Button className="border border-white hover:bg-white hover:text-black">
-                        Signup
-                        <LogIn className="w-4 h-4 ml-2" />
-                    </Button>
-                </Link>
+            <div className="right flex space-x-5">
+                {!isAuth && 
+                (
+                    <Link href="/signup">
+                        <Button className="border border-white hover:bg-white hover:text-black">
+                            Signup
+                            <LogIn className="w-4 ml-2" />
+                        </Button>
+                    </Link>
+                )}
+                <UserButton afterSignOutUrl="/" />
             </div>
         </div>
     );
