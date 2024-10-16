17d21e6e010d048a843f6597a8c1d3afb19db057
unknown91tech <116144227+unknown91tech@users.noreply.github.com>
2024-10-17 02:04:04 +0530

    Update ThemeProvider.tsx

--- src/app/ThemeProvider.tsx
+++ src/app/ThemeProvider.tsx
@@ -1,8 +1,8 @@
 "use client";
 import { NextUIProvider } from "@nextui-org/react";
 import {ThemeProvider as NextThemesProvider} from "next-themes";
-export default function ThemeProvider({children, ...props}) {
-  return <NextThemesProvider {...props}>
+export default function ThemeProvider({children}) {
+  return <NextThemesProvider>
     <NextUIProvider>{children}</NextUIProvider>
     </NextThemesProvider>;
 }
